75b490091c10e93b90d72fc2b15af6ab670fc66b
karthykarthick <karthylock@gmail.com>
2020-07-25 01:00:13 +0530

    add method to the code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,37 +1,35 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 def bubble_sort
+  puts 'Enter number of items'
 
-    puts "Enter number of items"
+  total_items = gets.chomp
 
-    total_items = gets.chomp
+  items = total_items.to_i
 
-    items=total_items.to_i
-    array = []
+  array = []
 
-    0.upto(items-1) do |i|
-        array[i] = gets.chomp
+  0.upto(items - 1) do |i|
+    array[i] = gets.chomp
+  end
+
+  puts "Total Number of items in the array is:  #{total_items}"
+
+  puts "Youre array is #{array}"
+
+  array.each do
+    i = 0
+    count = 0
+    while i < items - 1
+      array[i], array[i + 1] = array[i + 1], array[i] if array[i] > array[i + 1]
+      i += 1
+      count += 1
     end
+    break if count.zero?
+  end
 
-    puts "Total Number of items in the array is:  #{total_items}"
-
-    puts "Youre array is #{array}"
-
-    array.each do
-        i = 0
-        count = 0
-        while i < items- 1
-            if array[i] > array[i + 1]
-                array[i], array[i+1] = array[i+1], array[i]
-            end
-            i += 1
-            count += 1
-        end
-        break if count.zero?
-    end
-
-    puts "\nAnd sorted array is #{array}"
+  puts "\nAnd sorted array is #{array}"
 end
 
-bubble_sort()
-
+bubble_sort
