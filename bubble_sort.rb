702e9eb9975a372987e8a57b754563e1f2c0714b
karthykarthick <karthylock@gmail.com>
2020-07-24 22:02:18 +0530

    add method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
-puts "Enter number of items"
+def bubble_sort
 
-total_items = gets.chomp
+    puts "Enter number of items"
 
-i=total_items.to_i
-array_item = []
+    total_items = gets.chomp
 
-i.times do |i|
-    array_item[i] = gets.chomp
+    i=total_items.to_i
+    array_item = []
+
+    i.times do |i|
+        array_item[i] = gets.chomp
+    end
+
+    puts "Total Number of items in the array is:  #{total_items}"
+
+    puts "You're array is #{array_item}"
+
+    puts "\nAnd sorted array is #{array_item.sort}"
+
 end
-
-puts "Total Number of items in the array is:  #{total_items}"
-
-puts "You're array is #{array_item}"
-
-puts "\nAnd sorted array is #{array_item.sort}"
