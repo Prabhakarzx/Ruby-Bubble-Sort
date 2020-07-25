65ebc6a9292d7ea4291a3e3f6ed38d51a2ca101b
prabhakarzx <ppandey2098@gmail.com>
2020-07-26 03:37:12 +0530

    fix minor issues and linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,31 +1,28 @@
 def bubble_sort(array)
-	items=array.length
-	array.each do
-		i = 0
-		count = 0
-		while i < items - 1
-			array[i], array[i + 1] = array[i + 1], array[i] if array[i] > array[i + 1]
-			i += 1
-			count += 1
-		end
-		break if count.zero?
-	end
+  items=array.length
+  array.each do
+    i = 0
+    count = 0
+    while i < items - 1
+      array[i], array[i + 1] = array[i + 1], array[i] if array[i] > array[i + 1]
+      i += 1
+      count += 1
+    end
+    break if count.zero?
+  end
 end
+
 bubble_sort([3, 63, 9, 2, 0, 2])
 
 
 def bubble_sort_by(arr)
-	for i in 0..arr.length - 2 do
-		for j in 0..arr.length - 2 do
-			if (yield arr[j], arr[j + 1]).positive?
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-			end
-		end
-	end
-	return arr
+  for i in 0..arr.length - 2 do
+    if (yield arr[i], arr[i + 1]).positive?
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    end
+  end
+  arr
 end
 
-sorted_array = bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
-	left.length - right.length
-end
+bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
 
